189b1033f8eebba998a6898e6e60c3c474965b33
MerlinJu <m.jupe@gmx.de>
2026-06-09 21:40:01 +0200

    refacs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import AppShell from './components/layout/AppShell.vue'
+import RouterView from './components/layout/RouterView.vue'
 </script>
 
 <template>
-  <AppShell>
-    <RouterView />
-  </AppShell>
+  <AppShell />
+  <RouterView /> <!-- Router View component on right side of screen for navigation and routing -->
 </template>
